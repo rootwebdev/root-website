4b7b6a0bff39643c2adc1f54a6092cbfbf04372a
Filipe Medeiros <f.medeiros@campus.fct.unl.pt>
2020-02-19 14:48:04 +0100

    Props for animated styled-components

--- src/pages/directory/index.js
+++ src/pages/directory/index.js
@@ -1,3 +1,29 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-export default () => <div>I'm a page inside directory.</div>;
+// this is probably the best way to use animations in styled-components
+const slideBackAndForth = (yTranslate) => keyframes`
+    from {
+      transform: translate(0, 0);
+    }
+    
+    to {
+      transform: translate(300px, ${yTranslate}px);
+    }
+`;
+
+const AutoWidthDiv = styled.div`
+  width: auto;
+  display: inline-block;
+`;
+
+const SlidingDiv = styled(AutoWidthDiv)`
+  animation: ${props => slideBackAndForth(props.yTranslate)} 4s infinite ease-in-out alternate;
+`;
+
+export default () =>
+    <AutoWidthDiv>
+        I'm a page inside directory.
+        <br/>
+        <SlidingDiv yTranslate={200}>And I'm a custom div with styled-keyframes.</SlidingDiv>
+    </AutoWidthDiv>;
